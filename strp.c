2b41836d5596d170b1193b7230b83b4fb322c335
Marcin Cieslak <saper@saper.info>
2013-09-25 18:51:57 +0000

    Show failed pointer

--- strp.c
+++ strp.c
@@ -5,12 +5,18 @@ int
 main(int argc, char **argv)
 {
 	struct tm tp;
-	char *out;
+	char *out, *p;
 	if (argc != 3) {
 		fprintf(stderr, "Usage: strp date format\n");
 		return 255;
 	}
 	out = strptime(argv[1], argv[2], &tp);
 	fprintf(stderr, "%p\n", out);
+	if (out != NULL) {
+		fprintf(stderr, "%s\n", argv[1]);
+		for(p = argv[1];  *p && p < out; p++)
+			fprintf(stderr, " ");
+		fprintf(stderr, "^\n");
+	}
 	return 0;
 }
