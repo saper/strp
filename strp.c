0e23fb4d7cca542444745f2052c284b992efda98
Marcin Cieslak <saper@saper.info>
2013-09-25 18:57:52 +0000

    Print struct tm entries

--- strp.c
+++ strp.c
@@ -10,6 +10,10 @@ main(int argc, char **argv)
 		fprintf(stderr, "Usage: strp date format\n");
 		return 255;
 	}
+	tp.tm_year = 5555;
+	tp.tm_mon = 5555;
+	tp.tm_mday = 5555;
+	tp.tm_yday = 5555;
 	out = strptime(argv[1], argv[2], &tp);
 	fprintf(stderr, "%p\n", out);
 	if (out != NULL) {
@@ -17,6 +21,8 @@ main(int argc, char **argv)
 		for(p = argv[1];  *p && p < out; p++)
 			fprintf(stderr, " ");
 		fprintf(stderr, "^\n");
+		fprintf(stdout, "Year = %d, month = %d, day = %d, days = %d\n",
+			tp.tm_year, tp.tm_mon, tp.tm_mday, tp.tm_yday);
 	}
 	return 0;
 }
